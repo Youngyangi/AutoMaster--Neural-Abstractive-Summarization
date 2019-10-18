78fa012bf6b7df52996a5170b772aa0b60aca78d
Youngyang <Youngyang@whu.edu.cn>
2019-10-18 16:07:10 +0800

    Word2vec trained, with 30k+ vocabulary size.

--- train/Word2Vec.py
+++ train/Word2Vec.py
@@ -1,7 +1,8 @@
+import time
 from config import config
 from gensim.models import Word2Vec
 from gensim.models.fasttext import FastText
-from gensim.models import KeyedVectors
+from gensim.models.word2vec import LineSentence
 
 import os.path
 import pandas as pd
@@ -9,18 +10,13 @@ import pandas as pd
 
 def build_dataset(path):
     print(f"Read data from {path}")
-    seg_train_data = pd.read_csv(path, encoding='utf-8')
+    train_data = pd.read_csv(path, encoding='utf-8')
     stoplist = stopword(config.stop_word_path)
     lines = []
     for n in ['Input', 'Report']:
-        a = seg_train_data[n].values.tolist()
+        a = train_data[n].values.tolist()
         b = [str(x).split(' ') for x in a]
         lines.extend(b)
-    if not os.path.exists(config.train_contend_path):
-        with open(config.train_contend_path, 'w', encoding='utf-8') as f:
-            for x in lines:
-                for a in x:
-                    f.write(str(a)+'\n')
     return lines
 
 
@@ -34,7 +30,11 @@ def stopword(path):
 
 def build_word2vec(train_text, save_path):
     print("training word2vec...")
-    word2vec = FastText(train_text, sg=1, min_count=5, iter=5, size=200, workers=5)
+    start_time = time.time()
+    # Word2vec+LineSentence训练只用了30s不到，Fasttext要20分钟以上。
+    word2vec = Word2Vec(LineSentence(train_text), min_count=5, size=100, workers=5)
+    # word2vec = FastText(train_text, sg=1, min_count=5, iter=5, size=200, workers=5)
+    print(f'training is done, {time.time() - start_time} seconds elapsed')
     if os.path.exists(config.w2v_bin_path):
         print("already saved")
     else:
@@ -54,7 +54,10 @@ def build_vocab(wd2vc):
 
 if __name__ == '__main__':
     # train_texts = build_dataset(config.traindata_path)
-    # word2vec = build_word2vec(train_texts, config.w2v_bin_path)
-    w2v = FastText.load(config.w2v_bin_path)
+    # w2v = FastText.load(config.w2v_bin_path)
+    # vocab = build_vocab(w2v)
+    # word2vec = build_word2vec(config.corpus_path, config.w2v_bin_path)
+    w2v = Word2Vec.load(config.w2v_bin_path)
+    assert '汽车' in w2v.wv.vocab
     vocab = build_vocab(w2v)
-    print(vocab['<start>'])
+    print(len(vocab))
